c9161e6ceda9ab2d99d75d66d5d09c6524a7b2d4
Gizmotronn <droidology@allianceofdroids.org.au>
2022-02-22 21:05:09 +0800

    More content

--- scripts/run.js
+++ scripts/run.js
@@ -1,16 +1,20 @@
 const main = async () => {
     const [owner, randomPerson] = await hre.ethers.getSigners();
-    const domainContractFactory = await hre.ethers.getContractFactory("Domains");
-    const domainContract = await domainContractFactory.deploy();
+    const domainContractFactory = await hre.ethers.getContractFactory("Domains"); // rrobo is tld
+    const domainContract = await domainContractFactory.deploy('robo');
     await domainContract.deployed();
     console.log("Contract deployed to: ", domainContract.address);
     console.log("Contract deployed by: ", owner.address);
 
-    const txn = await domainContract.register("doom");
+    let txn = await domainContract.register("r2d2", { value: hre.ethers.utils.parseEther('0.1') });
+    // Matic variable
     await txn.wait();
 
-    const domainOwner = await domainContract.getAddress("doom");
-    console.log("Owner of domain:", domainOwner);
+    const address = await domainContract.getAddress("r2d2");
+    console.log("Owner of domain r2d2:", address);
+
+    const balance = await hre.ethers.provider.getBalance(domainContract.address);
+    console.log("Contract balance:", hre.ethers.utils.formatEther(balance));
 
     // (If) trying to set a record/domain that doesn't belong to account
     /*txn = await domainContract.connect(randomPerson).setRecord("doom", "empty");
@@ -28,3 +32,23 @@ const runMain = async () => {
 };
 
 runMain();
+
+/* SVG -> for NFT
+<svg xmlns="http://www.w3.org/2000/svg" width="270" height="270" fill="none">
+    <path fill="url(#B)" d="M0 0h270v270H0z"/>
+        <defs>
+            <filter id="A" color-interpolation-filters="sRGB" filterUnits="userSpaceOnUse" height="270" width="270">
+                <feDropShadow dx="0" dy="1" stdDeviation="2" flood-opacity=".225" width="200%" height="200%"/>
+            </filter>
+        </defs>
+    <path d="M72.863 42.949c-.668-.387-1.426-.59-2.197-.59s-1.529.204-2.197.59l-10.081 6.032-6.85 3.934-10.081 6.032c-.668.387-1.426.59-2.197.59s-1.529-.204-2.197-.59l-8.013-4.721a4.52 4.52 0 0 1-1.589-1.616c-.384-.665-.594-1.418-.608-2.187v-9.31c-.013-.775.185-1.538.572-2.208a4.25 4.25 0 0 1 1.625-1.595l7.884-4.59c.668-.387 1.426-.59 2.197-.59s1.529.204 2.197.59l7.884 4.59a4.52 4.52 0 0 1 1.589 1.616c.384.665.594 1.418.608 2.187v6.032l6.85-4.065v-6.032c.013-.775-.185-1.538-.572-2.208a4.25 4.25 0 0 0-1.625-1.595L41.456 24.59c-.668-.387-1.426-.59-2.197-.59s-1.529.204-2.197.59l-14.864 8.655a4.25 4.25 0 0 0-1.625 1.595c-.387.67-.585 1.434-.572 2.208v17.441c-.013.775.185 1.538.572 2.208a4.25 4.25 0 0 0 1.625 1.595l14.864 8.655c.668.387 1.426.59 2.197.59s1.529-.204 2.197-.59l10.081-5.901 6.85-4.065 10.081-5.901c.668-.387 1.426-.59 2.197-.59s1.529.204 2.197.59l7.884 4.59a4.52 4.52 0 0 1 1.589 1.616c.384.665.594 1.418.608 2.187v9.311c.013.775-.185 1.538-.572 2.208a4.25 4.25 0 0 1-1.625 1.595l-7.884 4.721c-.668.387-1.426.59-2.197.59s-1.529-.204-2.197-.59l-7.884-4.59a4.52 4.52 0 0 1-1.589-1.616c-.385-.665-.594-1.418-.608-2.187v-6.032l-6.85 4.065v6.032c-.013.775.185 1.538.572 2.208a4.25 4.25 0 0 0 1.625 1.595l14.864 8.655c.668.387 1.426.59 2.197.59s1.529-.204 2.197-.59l14.864-8.655c.657-.394 1.204-.95 1.589-1.616s.594-1.418.609-2.187V55.538c.013-.775-.185-1.538-.572-2.208a4.25 4.25 0 0 0-1.625-1.595l-14.993-8.786z" fill="#fff"/>
+    <defs>
+        <linearGradient id="B" x1="0" y1="0" x2="270" y2="270" gradientUnits="userSpaceOnUse">
+            <stop stop-color="#cb5eee"/><stop offset="1" stop-color="#0cd7e4" stop-opacity=".99"/>
+        </linearGradient>
+    </defs>
+    <text x="32.5" y="231" font-size="27" fill="#fff" filter="url(#A)" font-family="Plus Jakarta Sans,DejaVu Sans,Noto Color Emoji,Apple Color Emoji,sans-serif" font-weight="bold">
+        gizmotronn.robo
+    </text>
+</svg>
+*/
